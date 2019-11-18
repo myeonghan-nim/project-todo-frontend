4e6308853725fd6fff8acd555ae8bce36bb0d7b3
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-11-18 13:54:14 +0900

    set basic login logic

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-div col-6 offset-3">
+
+    <div class="form-group">
+      <label for="id">ID</label>
+      <input type="text" class="form-control" id="id">
+    </div>
+
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input type="password" class="form-control" id="password">
+    </div>
+
+    <button class="btn btn-primary">Login</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm'
+}
+</script>
+
+<style>
+
+</style>
