8cd4be3f7cf4dee740231e3882559a9ecb06498e
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-11-18 15:45:29 +0900

    set login session logic

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,24 +1,58 @@
 <template>
-  <div class="login-div col-6 offset-3">
+  <div>
 
-    <div class="form-group">
-      <label for="id">ID</label>
-      <input type="text" class="form-control" id="id">
+    <div class="spinner-border text-primary" role="status" v-if="loading">
+      <span class="sr-only">Loading...</span>
     </div>
 
-    <div class="form-group">
-      <label for="password">Password</label>
-      <input type="password" class="form-control" id="password">
-    </div>
+    <div class="login-div col-6 offset-3" v-else>
 
-    <button class="btn btn-primary">Login</button>
+      <div class="form-group">
+        <label for="id">ID</label>
+        <input type="text" class="form-control" id="id" v-model="credential.username">
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" v-model="credential.password">
+      </div>
+
+      <button class="btn btn-primary" @click="login">Login</button>
+
+    </div>
 
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'LoginForm'
+  name: 'LoginForm',
+
+  data: function() {
+    return {
+      credential: {
+        username: '',
+        password: ''
+      },
+      loading: false
+    }
+  },
+
+  methods: {
+    login() {
+      axios.post('http://localhost:8000/api-token-auth/', this.credential)
+        .then((r) => {
+          this.loading = true
+          console.log(r)
+        })
+        .catch((e) => {
+          this.loading = true
+          console.log(e)
+        })
+    }
+  }
 }
 </script>
 
