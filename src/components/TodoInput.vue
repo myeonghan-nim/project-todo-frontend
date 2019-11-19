ae14e80ae21f5037b6998300ae7add8219140720
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-11-19 15:59:20 +0900

    complete front create logic

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="todo-input my-2">
+    <div class="input-group">
+
+      <input type="text" class="form-control" v-model="title" @keydown.enter="createTodo">
+
+      <span class="input-group-btn">
+        <button class="btn btn-outline-secondary" @click="createTodo">➕</button>
+      </span>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoInput',
+
+  data() {
+    return {
+      title: ''
+    }
+  },
+
+  methods: {
+    createTodo() {
+      this.$emit('createTodo', this.title)
+      this.title = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
