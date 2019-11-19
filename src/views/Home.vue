74ff03e600eed0bb5cb6d6d2ea2db78a459cfe4e
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-11-19 15:12:11 +0900

    set data load logic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <TodoList/>
+    <TodoList :todos="todos"/>
   </div>
 </template>
 
 <script>
 import router from '../router/index.js'
 import TodoList from '../components/TodoList.vue'
+import jwtDecode from 'jwt-decode'
+import axios from 'axios'
 
 export default {
   name: "home",
 
   components: {
     TodoList
+  },
+
+  data() {
+    return {
+      todos: []
+    }
   },
 
   methods: {
@@ -23,11 +31,31 @@ export default {
         // redirect to login page
         router.push('/login')
       }
+    },
+
+    getTodos() {
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const decodedToken = jwtDecode(token)
+      const userId = decodedToken.user_id
+
+      const requestHeader = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+
+      axios.get(`http://localhost:8000/api/v1/users/${userId}/`, requestHeader)
+        .then((r) => {
+          this.todos = r.data.todo_set
+        })
+        .catch((e) => {console.log(e)})
     }
   },
 
   mounted: function() {
     this.checkLoggedIn()
+    this.getTodos()
   }
 }
 </script>
