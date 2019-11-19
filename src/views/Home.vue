02dc6abfd99cb7cda0c5c0244ed1d9acfe896646
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-11-19 15:59:20 +0900

    Change: todo create

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
+    <TodoInput @createTodo="createTodo"/>
     <TodoList :todos="todos"/>
   </div>
 </template>
 
 <script>
 import router from '../router/index.js'
+
 import TodoList from '../components/TodoList.vue'
+import TodoInput from '../components/TodoInput.vue'
+
 import jwtDecode from 'jwt-decode'
 import axios from 'axios'
 
@@ -14,7 +18,8 @@ export default {
   name: "home",
 
   components: {
-    TodoList
+    TodoList,
+    TodoInput
   },
 
   data() {
@@ -48,6 +53,30 @@ export default {
       axios.get(`http://localhost:8000/api/v1/users/${userId}/`, requestHeader)
         .then((r) => {
           this.todos = r.data.todo_set
+        })
+        .catch((e) => {console.log(e)})
+    },
+
+    createTodo(title) {
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const decodedToken = jwtDecode(token)
+      const userId = decodedToken.user_id
+
+      const requestHeader = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+
+      const requestForm = new FormData()
+
+      requestForm.append('user', userId)
+      requestForm.append('title', title)
+
+      axios.post('http://localhost:8000/api/v1/todos/', requestForm, requestHeader)
+        .then((r) => {
+          this.todos.push(r.data)
         })
         .catch((e) => {console.log(e)})
     }
