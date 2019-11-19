b6511585bddce542e195f59425e6620e80b0dc39
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-11-19 15:35:15 +0900

    Change: todo delete

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,7 +6,7 @@
     <div class="card m-1" v-for="todo in todos" :key="todo.id">
       <div class="card-body d-flex justify-content-between m-1">
         <span>{{todo.title}}</span>
-        <span>🗑️</span>
+        <span @click="deleteTodo(todo)">🗑️</span>
       </div>
     </div>
 
@@ -14,11 +14,39 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'TodoList',
 
   props: {
     todos: Array
+  },
+
+  methods: {
+    deleteTodo(todo) {
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const requestHeader = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+
+      axios.delete(`http://localhost:8000/api/v1/todos/${todo.id}/`, requestHeader)
+        .then(() => {
+          const targetTodo = this.todos.find(function(el) {
+            return el === todo
+          })
+
+          const idx = this.todos.indexOf(targetTodo)
+
+          if (idx > -1) {
+            this.todos.splice(idx, 1)
+          }
+        })
+        .catch((e) => {console.log(e)})
+    }
   }
 }
 </script>
